8a291dd614708770b024fe35ee402b438391a24d
madhrikakumanu <madhuriharibabu@gmail.com>
2018-10-14 18:45:41 -0600

    intial commit

---
+++ UILayer/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace UILayer
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+        public string GetUserDetails()
+        {
+            string ConString = @"Data Source=localhost;Initial Catalog=hrms;Integrated Security=True";
+            string sql = "Select * FROM [HRMS].[dbo].[User]";
+            SqlConnection con = new SqlConnection(ConString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql,con);
+ 
+            DataSet ds = new DataSet();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            da.Fill(ds);
+
+            string JSONString = string.Empty;
+            JSONString = JsonConvert.SerializeObject(ds);
+            return JSONString;  
+        }
+    }
+}
---
+++ UILayer/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UILayer.Startup))]
+namespace UILayer
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
